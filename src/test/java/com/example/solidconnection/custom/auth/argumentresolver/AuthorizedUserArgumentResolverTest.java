cb20e34688e115ea3b734b0474385d6c6ba78e58
nayonsoso <panda0329@naver.com>
2025-01-27 11:09:21 +0900

    test: argumentResolver 테스트 작성

--- src/test/java/com/example/solidconnection/custom/auth/argumentresolver/AuthorizedUserArgumentResolverTest.java
+++ src/test/java/com/example/solidconnection/custom/auth/argumentresolver/AuthorizedUserArgumentResolverTest.java
@@ -1,23 +1,18 @@
 package com.example.solidconnection.custom.auth.argumentresolver;
 
-import com.example.solidconnection.custom.auth.authentication.ValidAuthenticationToken;
+import com.example.solidconnection.custom.auth.authentication.SiteUserAuthentication;
 import com.example.solidconnection.custom.auth.userdetails.JwtUserDetails;
-import com.example.solidconnection.custom.exception.CustomException;
-import com.example.solidconnection.siteuser.domain.AuthType;
 import com.example.solidconnection.siteuser.domain.SiteUser;
 import com.example.solidconnection.siteuser.repository.SiteUserRepository;
 import com.example.solidconnection.support.TestContainerSpringBootTest;
-import com.example.solidconnection.type.Gender;
-import com.example.solidconnection.type.PreparationStatus;
-import com.example.solidconnection.type.Role;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 
-import static com.example.solidconnection.custom.exception.ErrorCode.AUTHENTICATION_FAILED;
+import static com.example.solidconnection.e2e.DynamicFixture.createSiteUser;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatCode;
 
 @TestContainerSpringBootTest
 @DisplayName("인증된 사용자 argument resolver 테스트")
@@ -29,12 +24,17 @@ class AuthorizedUserArgumentResolverTest {
     @Autowired
     private SiteUserRepository siteUserRepository;
 
+    @BeforeEach
+    void setUp() {
+        SecurityContextHolder.clearContext();
+    }
+
     @Test
     void security_context_에_저장된_인증된_사용자를_반환한다() throws Exception {
         // given
         SiteUser siteUser = siteUserRepository.save(createSiteUser());
         JwtUserDetails userDetails = new JwtUserDetails(siteUser);
-        ValidAuthenticationToken authentication = new ValidAuthenticationToken("token", userDetails);
+        SiteUserAuthentication authentication = new SiteUserAuthentication("token", userDetails);
         SecurityContextHolder.getContext().setAuthentication(authentication);
 
         // when
@@ -45,23 +45,8 @@ class AuthorizedUserArgumentResolverTest {
     }
 
     @Test
-    void security_context_에_저장된_사용자가_없으면_예외_응답을_반환한다() {
+    void security_context_에_저장된_사용자가_없으면_null_을_반환한다() throws Exception {
         // when, then
-        assertThatCode(() -> authorizedUserArgumentResolver.resolveArgument(null, null, null, null))
-                .isInstanceOf(CustomException.class)
-                .hasMessageContaining(AUTHENTICATION_FAILED.getMessage());
-    }
-
-    private SiteUser createSiteUser() {
-        return new SiteUser(
-                "email",
-                "nickname",
-                "profileImageUrl",
-                "1999-01-01",
-                PreparationStatus.CONSIDERING,
-                Role.MENTEE,
-                Gender.MALE,
-                AuthType.KAKAO
-        );
+        assertThat(authorizedUserArgumentResolver.resolveArgument(null, null, null, null)).isNull();
     }
 }
---
+++ src/test/java/com/example/solidconnection/custom/auth/argumentresolver/ExpirationIgnoredTokenResolverTest.java
@@ -0,0 +1,37 @@
+package com.example.solidconnection.custom.auth.argumentresolver;
+
+import com.example.solidconnection.custom.auth.authentication.ExpirationIgnoredToken;
+import com.example.solidconnection.support.TestContainerSpringBootTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@TestContainerSpringBootTest
+@DisplayName("만료 시간을 검증하지 않는 토큰 argument resolver 테스트")
+class ExpirationIgnoredTokenResolverTest {
+
+    @Autowired
+    private ExpirationIgnoredTokenResolver expirationIgnoredTokenResolver;
+
+    @Test
+    void security_context_에_저장된_만료시간을_검증하지_않는_토큰을_반환한다() throws Exception {
+        // given
+        ExpirationIgnoredToken authentication = new ExpirationIgnoredToken("token");
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        // when
+        ExpirationIgnoredToken expirationIgnoredToken = (ExpirationIgnoredToken) expirationIgnoredTokenResolver.resolveArgument(null, null, null, null);
+
+        // then
+        assertThat(expirationIgnoredToken.getToken()).isEqualTo("token");
+    }
+
+    @Test
+    void security_context_에_저장된_만료시간을_검증하지_않는_토큰이_없으면_null_을_반환한다() throws Exception {
+        // when, then
+        assertThat(expirationIgnoredTokenResolver.resolveArgument(null, null, null, null)).isNull();
+    }
+}
